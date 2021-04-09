c2b468d93542652b970114a1b86d026bdbabef37
coskunbicici <coskun.bicici@gmail.com>
2021-04-09 15:40:23 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,86 @@
+﻿using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            InMemCarDal carDal = new InMemCarDal();
+            WriteTitle("GetById(1)");
+            var carById = carDal.GetById(1);
+            Console.WriteLine("| " + "{0,-6}" + " | " + "{1,-9}" + " | " + "{2,-9}" + " | " + "{3,-9}" + " | " + "{4,-10}" + " | " + "{5,-50}" + " | ",
+                                        "Id", "BrandId", "ColorId", "ModelYear", "DailyPrice", "Description");
+            Console.WriteLine("| " + "{0,-6}" + " | " + "{1,-9}" + " | " + "{2,-9}" + " | " + "{3,-9}" + " | " + "{4,-10}" + " | " + "{5,-50}" + " | ",
+                                   carById.Id, carById.BrandId, carById.ColorId, carById.ModelYear, carById.DailyPrice, carById.Description);
+            WriteSeperate();
+
+
+            WriteTitle("GetAll()");
+            GetAllCars(carDal);
+
+
+            WriteTitle("Add(6)");
+            var addCar = new Car
+            {
+                Id = 6,
+                BrandId = 3,
+                ColorId = 3,
+                ModelYear = 2019,
+                DailyPrice = 100,
+                Description = "Araba 6"
+            };
+            carDal.Add(addCar);
+            GetAllCars(carDal);
+
+
+            WriteTitle("Update(5)");
+            var updateCar = new Car
+            {
+                Id = 5,
+                BrandId = 3,
+                ColorId = 3,
+                ModelYear = 2019,
+                DailyPrice = 105,
+                Description = "Araba 555"
+            };
+            carDal.Update(updateCar);
+            GetAllCars(carDal);
+
+
+            WriteTitle("Delete(5)");
+            var deleteCar = new Car
+            {
+                Id = 5
+            };
+            carDal.Delete(deleteCar);
+            GetAllCars(carDal);
+        }
+
+        private static void WriteTitle(string title)
+        {
+            Console.WriteLine($"++++++++++ {title} ++++++++++");
+        }
+
+        private static void WriteSeperate()
+        {
+            Console.WriteLine("------------------------------");
+        }
+
+        private static void GetAllCars(InMemCarDal carDal)
+        {
+            Console.WriteLine("| " + "{0,-6}" + " | " + "{1,-9}" + " | " + "{2,-9}" + " | " + "{3,-9}" + " | " + "{4,-10}" + " | " + "{5,-50}" + " | ",
+                            "Id", "BrandId", "ColorId", "ModelYear", "DailyPrice", "Description");
+            var carList = carDal.GetAll();
+            foreach (var carItem in carList)
+            {
+                Console.WriteLine("| " + "{0,-6}" + " | " + "{1,-9}" + " | " + "{2,-9}" + " | " + "{3,-9}" + " | " + "{4,-10}" + " | " + "{5,-50}" + " | ",
+                                   carItem.Id, carItem.BrandId, carItem.ColorId, carItem.ModelYear, carItem.DailyPrice, carItem.Description);
+            }
+            WriteSeperate();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemCarDal.cs
@@ -0,0 +1,60 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemCarDal
+    {
+        List<Car> _cars;
+        public InMemCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=1,ModelYear=2020,DailyPrice=150,Description="Araba 1"},
+                new Car{Id=2,BrandId=2,ColorId=1,ModelYear=2019,DailyPrice=150,Description="Araba 2"},
+                new Car{Id=3,BrandId=2,ColorId=1,ModelYear=2021,DailyPrice=150,Description="Araba 3"},
+                new Car{Id=4,BrandId=3,ColorId=2,ModelYear=2020,DailyPrice=150,Description="Araba 4"},
+                new Car{Id=5,BrandId=3,ColorId=2,ModelYear=2021,DailyPrice=150,Description="Araba 5"}
+            };
+        }
+
+        // GetById, GetAll, Add, Update, Delete
+
+        public Car GetById(int id)
+        {
+            return _cars.SingleOrDefault(c => c.Id == id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+
+    }
+}
